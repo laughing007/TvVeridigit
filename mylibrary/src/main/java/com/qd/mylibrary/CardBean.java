10f99dfded9f5cc8293a3f0a904fe3f6b1e22a51
laughing007 <sunxikai000@163.com>
2024-12-18 15:34:40 +0800

    Add files via upload

---
+++ mylibrary/src/main/java/com/qd/mylibrary/CardBean.java
@@ -0,0 +1,59 @@
+package com.qd.mylibrary;
+
+public class CardBean {
+
+    private String type;
+    private String originUrl;
+    private int pixelX;
+    private int pixelY;
+    private int width;
+    private int height;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOriginUrl() {
+        return originUrl;
+    }
+
+    public void setOriginUrl(String originUrl) {
+        this.originUrl = originUrl;
+    }
+
+    public int getPixelX() {
+        return pixelX;
+    }
+
+    public void setPixelX(int pixelX) {
+        this.pixelX = pixelX;
+    }
+
+    public int getPixelY() {
+        return pixelY;
+    }
+
+    public void setPixelY(int pixelY) {
+        this.pixelY = pixelY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
